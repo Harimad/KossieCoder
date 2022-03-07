85d50483ca01fd2e24576097868756576e8790f1
Harimad <leesh68481@gmail.com>
2022-03-07 21:21:45 +0900

    05 폼에서 useState 사용하기

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,32 +1,28 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [text, setText] = useState('TimLee')
-  const onSubmit = () => {
-    alert('submitted')
-  }
-
-  const onKeyUp = e => {
-    //Enter 누르면
-    if (e.keyCode === 13) onSubmit()
-  }
-
-  const updateText = () => {
-    setText('New TimLee')
-    console.log(text)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const onSubmit = event => {
+    event.preventDefault()
+    console.log(username, password)
   }
 
   return (
     <div className="App">
-      <input onKeyUp={onKeyUp} />
-      <button onClick={onSubmit}>Submit</button>
-
-      <br />
-      <br />
-      <br />
-
-      <span></span>
-      <button onClick={updateText}>Update</button>
+      <form onSubmit={onSubmit}>
+        <input
+          placeholder="Username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <input
+          placeholder="Password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
   )
 }
