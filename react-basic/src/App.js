6385b4a5e3068d1f4904ef6d4fc7c2a5ecc08a88
Harimad <leesh68481@gmail.com>
2022-03-08 14:00:05 +0900

    06 useEffect 사용하기

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,28 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const onSubmit = event => {
-    event.preventDefault()
-    console.log(username, password)
-  }
+  const [count, setCount] = useState(0);
+  const [timLee, setTimLee] = useState(100);
+
+  //처음 랜더링때 실행, 이후 리랜더링때 마다 실행
+  useEffect(() => { console.log(`두번째 인자 없음 / count:${count}, timLee: ${timLee}`); })
+
+   //처음 랜더링때만 실행
+  useEffect(() => { console.log(`두번째 인자가 []일때 /count:${count}, timLee: ${timLee}`); }, [])
+
+   //처음 랜더링때 실행, 이후 count값만 바뀔때마다 실행
+  useEffect(() => { console.log(`두번째 인자가 [count]일때 /count:${count}, timLee: ${timLee}`); }, [count])
 
   return (
     <div className="App">
-      <form onSubmit={onSubmit}>
-        <input
-          placeholder="Username"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-        />
-        <input
-          placeholder="Password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
-      </form>
+      <button onClick={() => {setCount(count+1)}}>count</button>
+      <button onClick={() => {setTimLee(timLee+1)}}>timLee</button>
     </div>
   )
 }
