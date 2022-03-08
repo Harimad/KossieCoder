7b63278fbf69857680f4ded3a211dc2afe31bf0d
Harimad <leesh68481@gmail.com>
2022-03-08 14:20:11 +0900

    07 컴포넌트로 반복 제거

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,22 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import BtnCounter from './btnCounter'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [timLee, setTimLee] = useState(100);
-
-  //처음 랜더링때 실행, 이후 리랜더링때 마다 실행
-  useEffect(() => { console.log(`두번째 인자 없음 / count:${count}, timLee: ${timLee}`); })
-
-   //처음 랜더링때만 실행
-  useEffect(() => { console.log(`두번째 인자가 []일때 /count:${count}, timLee: ${timLee}`); }, [])
-
-   //처음 랜더링때 실행, 이후 count값만 바뀔때마다 실행
-  useEffect(() => { console.log(`두번째 인자가 [count]일때 /count:${count}, timLee: ${timLee}`); }, [count])
-
   return (
     <div className="App">
-      <button onClick={() => {setCount(count+1)}}>count</button>
-      <button onClick={() => {setTimLee(timLee+1)}}>timLee</button>
+      <BtnCounter></BtnCounter>
+      <BtnCounter></BtnCounter>
+      <BtnCounter></BtnCounter>
     </div>
   )
 }
