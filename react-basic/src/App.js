cf0a4c6080fa37387825cb1d8f60d32dd1c0377f
Harimad <leesh68481@gmail.com>
2022-03-08 17:04:30 +0900

    10 리액트 반복문

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,15 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import BtnCounter from './btnCounter'
+import React from 'react'
 
 function App() {
-  const [condition, setCondidtion] = useState(false);
-  const toggle = () => setCondidtion(!condition);
-  const renderCondition = condition ? 'true' : 'false';
+  const movies = [
+    {title: 'TimLee1', year: 2000},
+    {title: 'TimLee2', year: 2001},
+    {title: 'TimLee3', year: 2002},
+    {title: 'TimLee4', year: 2003},
+    {title: 'TimLee5', year: 2004},
+  ];
+  const renderMovies = movies.map(movie => {
+    return (
+      <div className='movie' key={movie.title}>
+        <div className='movie-title'>{movie.title}</div>
+        <div className='movie-year'>{movie.year}</div>
+      </div>
+    )
+  })
 
   return (
     <div className="App">
-      <div>{renderCondition}</div>
-      <button onClick={toggle}>Toggle</button>
+      <h1>Movie List</h1>
+      {renderMovies}
     </div>
   )
 }
