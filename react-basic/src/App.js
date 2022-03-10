99c7d0b2f290f338d888ee09c0c9c163ee6fae5c
Harimad <leesh68481@gmail.com>
2022-03-10 16:00:31 +0900

    19 네비게이션 바 만들기

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Movie from './components/Movie'
 import MovieForm from './components/MovieForm'
+import Navbar from './components/Navbar'
 
 function App() {
   const [movies, setMovies] = useState([])
@@ -22,6 +23,7 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <h1>Movie List</h1>
       <MovieForm addMovie={addMovie} />
       {renderMovies}
