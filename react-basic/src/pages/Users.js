3e07bb13691dcde17e3486c5674fc97691d044dd
Harimad <leesh68481@gmail.com>
2022-03-10 18:57:28 +0900

    24 유저 데이터 화면에 출력하기

--- react-basic/src/pages/Users.js
+++ react-basic/src/pages/Users.js
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
+import UserList from '../components/UserList'
 
 const Users = () => {
+  const [users, setUsers] = useState([])
+
   useEffect(() => {
     Axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
-      console.log(response)
+      setUsers(response.data)
     })
   }, [])
   return (
     <>
       <h1>Users</h1>
+      <UserList users={users} />
     </>
   )
 }
