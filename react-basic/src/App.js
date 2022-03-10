c5e49226edc9f3d98adf65c7cb724bf57c0a2700
Harimad <leesh68481@gmail.com>
2022-03-10 10:31:09 +0900

    14 자식 컴포넌트에서 부모 컴포넌트 스테이트 변경하기

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react'
 import Movie from './components/Movie'
+import MovieForm from './components/MovieForm'
 
 function App() {
-  const [movieTitle, setMovieTitle] = useState('')
-  const [movieYear, setMovieYear] = useState('')
   const [movies, setMovies] = useState([
     { title: 'TimLee1', year: 2000 },
     { title: 'TimLee2', year: 2001 },
@@ -11,33 +10,19 @@ function App() {
     { title: 'TimLee4', year: 2003 },
     { title: 'TimLee5', year: 2004 },
   ])
-  const onSubmit = e => {
-    e.preventDefault()
-    setMovies([...movies, { 'title': movieTitle, 'year': movieYear }])
-    setMovieTitle('')
-    setMovieYear('')
-  }
 
   const renderMovies = movies.map((movie, idx) => {
     return <Movie movie={movie} key={idx} />
   })
 
+  const addMovie = movie => {
+    setMovies([...movies, movie])
+  }
+
   return (
     <div className="App">
       <h1>Movie List</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          placeholder="MovieTitle"
-          value={movieTitle}
-          onChange={e => setMovieTitle(e.target.value)}
-        />
-        <input
-          placeholder="MovieYear"
-          value={movieYear}
-          onChange={e => setMovieYear(e.target.value)}
-        />
-        <button type="submit">영화추가</button>
-      </form>
+      <MovieForm addMovie={addMovie} />
       {renderMovies}
     </div>
   )
---
+++ react-basic/src/components/MovieForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+const MovieForm = ({ addMovie }) => {
+  const [movieTitle, setMovieTitle] = useState('')
+  const [movieYear, setMovieYear] = useState('')
+
+  const resetInput = () => {
+    setMovieTitle('')
+    setMovieYear('')
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+    addMovie({ title: movieTitle, year: movieYear })
+    resetInput()
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        placeholder="MovieTitle"
+        value={movieTitle}
+        onChange={e => setMovieTitle(e.target.value)}
+      />
+      <input
+        placeholder="MovieYear"
+        value={movieYear}
+        onChange={e => setMovieYear(e.target.value)}
+      />
+      <button type="submit">영화추가</button>
+    </form>
+  )
+}
+
+export default MovieForm
