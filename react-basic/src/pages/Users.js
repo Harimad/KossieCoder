d7afdc29ca4d5ef2418c937576d812b2c7c6c4d5
Harimad <leesh68481@gmail.com>
2022-03-10 19:34:49 +0900

    26 로딩 Spinner 추가하기

--- react-basic/src/pages/Users.js
+++ react-basic/src/pages/Users.js
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 import UserList from '../components/UserList'
+import Spinner from '../components/Spinner'
 
 const Users = () => {
   const [users, setUsers] = useState([])
+  const [spinner, setSpinner] = useState(true)
 
   useEffect(() => {
     Axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
       setUsers(response.data)
+      setSpinner(!spinner)
     })
   }, [])
   return (
     <>
       <h1>Users</h1>
-      <UserList users={users} />
+      {spinner ? <Spinner /> : <UserList users={users} />}
     </>
   )
 }
